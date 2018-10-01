7e29545e2ce1a56e1e15213018a7ae5218000ad6
sandeeraDS <sandeerads@gmail.com>
2018-10-01 06:21:20 +0530

    first

---
+++ routing/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { ServersService } from './servers/servers.service';
+import { importExpr } from '@angular/compiler/src/output/output_ast';
+import {Routes, RouterModule} from '@angular/router'
+
+const appRoute:Routes=[
+  {path:'' , component:HomeComponent},
+  {path:'users' , component:UsersComponent},
+  {path:'users/:id/:name' , component:UserComponent},
+  {path: 'servers' , component:ServersComponent},
+  {path: 'servers/:id/edit' , component:EditServerComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    EditServerComponent,
+    ServerComponent,
+    UsersComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoute)
+  ],
+  providers: [ServersService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
